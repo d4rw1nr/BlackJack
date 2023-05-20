24285e182dba997a7f0ad47f81e3b05765727171
d4rw1nr <darwinjo6.4@gmail.com>
2023-05-19 23:20:23 -0500

    Changes of the structure of the game

--- BlackJack.py
+++ BlackJack.py
@@ -3,11 +3,13 @@ from Actions import deal
 from Actions import hit
 
 # Se crea la baraja de cartas
-deck_values = {"A":11, "2": 2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K":10}
 
 deck = ["A","2","3","4","5","6","7","8","9","10","J","Q","K",]*4
 
 def play(deck:list):
+    
+    values = {"A":11, "2": 2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K":10}
+
     shuffle_deck(deck)
 
     croupier = []
@@ -16,7 +18,14 @@ def play(deck:list):
     deal(deck, croupier, player)
     print("Croupier cards:" + str(croupier))
     print("Player cards:" + str(player))
-    
+
 
 
 play(deck)
+
+
+def hard_total(deck:list,croupier:list, player:list, surrender:bool):
+    values = {"A":11, "2": 2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K":10}
+
+    if ((values[player[0]] + values[player[1]]) >= 17):
+        return 
