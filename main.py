7dfeb78c65938c397f90f85f779d0c7e22e9600a
d4rw1nr <darwinjo6.4@gmail.com>
2023-08-23 18:43:39 -0500

    structure for bot iteration

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from controller import BlackjackGame
 
 def main():
-    games = 1
+    games = 1000
     while games != 0:
-        game = BlackjackGame(bot=True, bot_balance=100, bot_bet = 10, bot_rounds=10)
+        game = BlackjackGame(bot=True, bot_balance=100, bot_bet = 10, bot_rounds=500)
         game.play()
         games -= 1
 
